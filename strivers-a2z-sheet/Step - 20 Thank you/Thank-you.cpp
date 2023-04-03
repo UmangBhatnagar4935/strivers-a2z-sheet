c72cb06ef786652cc44c90d6354158828ebf134e
Sane Aalam <123074523+sane-aalam@users.noreply.github.com>
2023-04-03 15:36:45 +0530

    Add files via upload

---
+++ strivers-a2z-sheet/Step - 20 Thank you/Thank-you.cpp
@@ -0,0 +1,9 @@
+
+
+
+
+
+// Thank you @Raj Vikramaditya A.K.A Striver,
+// Thank you @saneAalam
+
+// mission 45Day's
---
+++ strivers-a2z-sheet/Step - 20 Thank you/massage-for-you.cpp
@@ -0,0 +1,5 @@
+// Hurrah!! 
+// You are ready for your placement after some months of hard work! 
+// All the best, keep striving… 
+
+// Striver
